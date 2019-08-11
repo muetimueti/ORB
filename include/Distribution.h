cf830b6c4bd88b72aeb5172b57225ea710e9ba2b
muetimueti <da_viech@yahoo.de>
2019-08-11 13:58:29 +0200

    moved round

--- include/Distribution.h
+++ include/Distribution.h
@@ -73,15 +73,6 @@ protected:
             int N, float epsilon, float threshold);
 };
 
-CV_INLINE  int myRound( float value )
-{
-#if defined HAVE_LRINT || defined CV_ICC || defined __GNUC__
-    return (int)lrint(value);
-#else
-    // not IEEE754-compliant rounding
-      return (int)(value + (value >= 0 ? 0.5f : -0.5f));
-#endif
-}
 
 template <typename T>
 struct PointCloud
